b66a33c400c4792ca6276c9ebacd92445c78c1c4
huayunh <96_mouse@sina.com>
2017-12-01 11:03:48 -0500

    add an outer ring for d3

--- js/facts.js
+++ js/facts.js
@@ -18,18 +18,35 @@ function d3_myDay() {
 	var defaultText = "Hover and click to see what happens!";
   	$('#chart1+.tooltip').text(defaultText).css('opacity', 0.5);
 
+  	// data for the inner ring
 	var data = [
-		{ label: 'on homework',        count: 8,
-			sub: {
-				label: '', count: 3
-			}},
-		{ label: 'in classes',         count: 4 },
-		{ label: 'in design critique', count: 1 },
-		{ label: 'on Wikipedia',       count: 1 },
-		{ label: 'in commuting',       count: 1 },
-		{ label: 'eating',             count: 2 },
-		{ label: 'sleeping',           count: 7 }
-	]
+		{ label: 'on homework',        count: 8, class: 'hw'},
+		{ label: 'in classes',         count: 4, class: 'cl'},
+		{ label: 'in design critique', count: 1, class: 'dc'},
+		{ label: 'on Wikipedia',       count: 1, class: 'wp'},
+		{ label: 'in commuting',       count: 1, class: 'ic'},
+		{ label: 'eating',             count: 2, class: 'ea'},
+		{ label: 'sleeping',           count: 7, class: 'sl'}, 
+	];
+
+	// data for the outer ring
+	var subData = [
+		{label: 'in reading',          count: 3,   class:'hw'},
+		{label: 'in thinking',         count: 2,   class:'hw'},
+		{label: 'in coding',           count: 3,   class:'hw'},
+		{label: 'without coffee',      count: 3.9, class:'cl'},
+		{label: 'with coffee',         count: 0.1, class:'cl'},
+		{label: 'convince them',       count: 0.6, class:'dc'},
+		{label: 'convinced',           count: 0.4, class:'dc'},
+		{label: 'edit',                count: 0.5, class:'wp'},
+		{label: 'edit war',            count: 0.5, class:'wp'},
+		{label: 'going to the campus', count: 0.8, class:'ic'},
+		{label: 'going back',          count: 0.2, class:'ic'}, 
+		{label: 'lunch',               count: 1,   class:'ea'},
+		{label: 'dinner',              count: 1,   class:'ea'},
+		{label: 'day dreaming',        count: 1,   class:'sl'},
+		{label: 'night dreaming',      count: 6,   class:'sl'}
+	];
 
 	var canvasWidth = $('#chart1').width();
 	var canvasHeight = parseInt($('#chart1').css('height'));
@@ -38,9 +55,7 @@ function d3_myDay() {
 
 	var radius = Math.min(width, height) / 2;
 
-	var color = d3.scaleOrdinal(d3.schemeCategory20c)
-	// var color = d3.scaleOrdinal()
-				  // .range(['#ff6666', '#e77', '#bc8282', '#249be5', '#4a8fba', '#668fa8', '#788791']);
+	var color = d3.scaleOrdinal(d3.schemeCategory20c);
 
 	var svg = d3.select('#chart1')
 				.append('svg')
@@ -52,12 +67,16 @@ function d3_myDay() {
 	var arc = d3.arc()
 				.innerRadius(radius*0.3)
 				.outerRadius(radius);
+	var arc2= d3.arc()
+				.innerRadius(radius)
+				.outerRadius(canvasWidth/2);
 
 	var pie = d3.pie()
 				.value(function(d) { return d.count; })
 				.sort(null);
 
-	var path = svg.selectAll('path')
+	var path = svg.append('g')
+				  .selectAll('path')
 				  .data(pie(data))
 				  .enter()
 				  .append('path')
@@ -72,10 +91,32 @@ function d3_myDay() {
 				  .on('mouseout', function() {
 				  	$('#chart1+.tooltip').text(defaultText).css('opacity', 0.5);
 				  })
-				  .on('click', function (){
-				  	console.log("wer");
-
+				  .on('click', function (d){
+				  	$('.sub path').addClass('hidden');
+				  	$('.'+d.data.class).removeClass('hidden');
 				  });
+
+	svg.append('g')
+	   .classed('sub', true)
+	   .selectAll('path')
+	   .data(pie(subData))
+	   .enter()
+	   .append('path')
+	   .attr('d', arc2)
+	   .attr('fill', function(d, i) {
+			return color(d.data.label);
+		})
+	   .attr('class', function(d) {
+	   		return d.data.class;
+	    })  
+	   .classed('hidden', true)
+	   .on('mouseover', function(d) {
+			$('#chart1+.tooltip').text("Time spent "+d.data.label+": "+d.data.count+" hours")
+								 .css('opacity', 1);
+			})
+	   .on('mouseout', function() {
+		 	$('#chart1+.tooltip').text(defaultText).css('opacity', 0.5);
+	    })
 
 
 }
